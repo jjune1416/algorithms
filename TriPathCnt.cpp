2692ce54479e9e7ab28189cf67f7963cca6e19e6
“INJUNE <jjune1416@gmail.com>
2018-01-27 20:05:47 +0900

    TriPathCnt.cpp first commit

---
+++ TriPathCnt.cpp
@@ -0,0 +1,55 @@
+//
+//  TriPathCnt.cpp
+//  TriPathCnt
+//
+//  Created by Ghost on 2018. 1. 27..
+//  Copyright © 2018년 Ghost. All rights reserved.
+//
+
+#include <iostream>
+#include <algorithm>
+
+const int MAX = 100;
+
+int height;
+int triangle[MAX][MAX];
+int maxMemo[MAX][MAX];
+int pathMemo[MAX][MAX];
+
+int TrianglePath(int y, int x)
+{
+    int& ret = maxMemo[y][x];
+    if(ret != -1) return ret;
+    if(y == height - 1) return ret = triangle[y][x];
+    return ret = triangle[y][x] + std::max(TrianglePath(y+1, x), TrianglePath(y+1, x+1));
+}
+
+int TriPathCnt(int y, int x)
+{
+    int& ret = pathMemo[y][x];
+    if(ret != -1) return ret;
+    if(y == height - 1) return ret = 1;
+    
+    ret = 0;
+    int max = std::max(maxMemo[y+1][x], maxMemo[y+1][x+1]);
+    for(int i = 0 ; i <= 1; i++)
+        if(max == maxMemo[y+1][x+i])
+            ret += TriPathCnt(y+1, x+i);
+    return ret;
+}
+
+int main()
+{
+    int TC;
+    std::cin >> TC;;
+    while(TC--)
+    {
+        std::cin >> height;
+        for(int i = 0 ; i < height; i++)
+            for(int j = 0 ; j <= i ; j++)
+                std::cin >> triangle[i][j], pathMemo[i][j] = maxMemo[i][j] = -1;
+        TrianglePath(0,0);
+        std::cout << TriPathCnt(0,0) << std::endl;
+    }
+    return 0;
+}
