f8ab827170d62ebd0d16f0ea04cc50aa6dc25483
“INJUNE <jjune1416@gmail.com>
2018-01-28 16:22:28 +0900

    add problem

--- CrackingCodingInterview/1-1 UniqueCharater.cpp
+++ CrackingCodingInterview/1-1 UniqueCharater.cpp
@@ -2,9 +2,13 @@
 //  1-1 UniqueCharater.cpp
 //  CrackTheCodingInterview
 //
+//  문자열에 포함된 문자들이 전부 유일한지를 검사하는 알고리즘을 구현하라.
+//  다른 자료구조를 사용할 수 없는 상황이라면 어떻게 하겠는가?
+//
 //  Created by Ghost on 2018. 1. 28..
 //  Copyright © 2018년 Ghost. All rights reserved.
 //
+//  - ASCII 인지 확인 필요
 
 #include <string>
 
