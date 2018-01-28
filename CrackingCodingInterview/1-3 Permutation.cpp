c654663de9486b03459828b720b8adfeda4a943b
“INJUNE <jjune1416@gmail.com>
2018-01-28 18:18:01 +0900

    permutation first commit

---
+++ CrackingCodingInterview/1-3 Permutation.cpp
@@ -0,0 +1,39 @@
+//
+//  1-3 Permutation.cpp
+//  CrackTheCodingInterview
+//
+//  문자열 두개를 입력으로 받아 그중 하나가 다른 하나의 순열인지 판별하는 메서드를 작성하라.
+//
+//  Created by Ghost on 2018. 1. 28..
+//  Copyright © 2018년 Ghost. All rights reserved.
+//
+
+#include <algorithm>
+#include <string>
+
+// nlgn
+bool Permutation1(std::string a, std::string b)
+{
+    if(a.size() != b.size())    return false;
+    std::sort(a.begin(), a.end());
+    std::sort(b.begin(), b.end());
+    return a == b;
+}
+
+// n+k
+bool Permutation2(std::string a, std::string b)
+{
+    if(a.size() != b.size())    return false;
+    int aCnt[256] = {0,};
+    int bCnt[256] = {0,};
+    for(int i = 0 ;i < a.size(); i++)
+    {
+        aCnt[a[i]]++;
+        bCnt[b[i]]++;
+    }
+    
+    for(int i = 0 ;i < 256; i++)
+        if(aCnt[i] != bCnt[i])
+            return false;
+    return true;
+}
