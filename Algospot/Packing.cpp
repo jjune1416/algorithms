d6a019b54c4e3300269655a1bf1e333f4946a419
“INJUNE <jjune1416@gmail.com>
2018-02-01 00:34:14 +0900

    optimize

--- Algospot/Packing.cpp
+++ Algospot/Packing.cpp
@@ -9,7 +9,7 @@
 #include <iostream>
 #include <string>
 
-const int MAX_ITEM = 101;
+const int MAX_ITEM = 100;
 const int MAX_VOLUME = 1001;
 
 struct _item
@@ -29,29 +29,35 @@ struct _item
 int itemCount;
 int totalVolume;
 int memoH[MAX_ITEM][MAX_VOLUME];
-int memoN[MAX_ITEM][MAX_VOLUME];
+int memoT[MAX_ITEM][MAX_VOLUME];
 int memoC[MAX_ITEM][MAX_VOLUME];
 _item item[MAX_ITEM];
 
 int GetMaxHopeness(int itemNum, int leftVolume)
 {
-    if(itemNum > itemCount) return -1;
+    if(leftVolume < 0)      return -987654321;
+    if(itemNum >= itemCount) return 0;
     
     int ret = memoH[itemNum][leftVolume];
     if(ret != -1)   return ret;
     
-    ret = 0;
-    for(int i = itemNum + 1 ; i <= itemCount; i++)
+    
+    int retInclude = item[itemNum].hopeness + GetMaxHopeness(itemNum + 1, leftVolume - item[itemNum].volume);
+    int retExclude = GetMaxHopeness(itemNum + 1, leftVolume);
+    
+    if(retInclude > retExclude)
     {
-        if(item[i].volume > leftVolume) continue;
-        int new_ret = item[i].hopeness + GetMaxHopeness(i, leftVolume - item[i].volume);
-        if(ret < new_ret)
-        {
-            ret = new_ret;
-            memoN[itemNum][leftVolume] = i;
-            memoC[itemNum][leftVolume] = memoC[i][leftVolume - item[i].volume] + 1;
-        }
+        ret = retInclude;
+        memoC[itemNum][leftVolume] = memoC[itemNum + 1][leftVolume - item[itemNum].volume] + 1;
+        memoT[itemNum][leftVolume] = 1;
     }
+    else
+    {
+        ret = retExclude;
+        memoC[itemNum][leftVolume] = memoC[itemNum + 1][leftVolume];
+        memoT[itemNum][leftVolume] = 0;
+    }
+    
     return ret;
 }
 
@@ -62,8 +68,7 @@ int main()
     while(TC--)
     {
         std::cin >> itemCount >> totalVolume;
-        item[0].set("", 0, 0);
-        for(int i = 1 ; i <= itemCount; i++)
+        for(int i = 0 ; i < itemCount; i++)
         {
             std::string name;
             int volume;
@@ -72,23 +77,22 @@ int main()
             item[i].set(name, volume, hopeness);
         }
         
-        for(int i = 0 ;i <= itemCount ;i++)
+        for(int i = 0 ;i < itemCount ;i++)
         {
-            for(int j = 0 ;j <=  totalVolume; j++)
+            for(int j = 0 ;j <= totalVolume; j++)
             {
-                memoH[i][j] = memoN[i][j] = - 1;
+                memoH[i][j] = memoT[i][j] = -1;
                 memoC[i][j] = 0;
             }
         }
     
         std::cout << GetMaxHopeness(0, totalVolume) << " " << memoC[0][totalVolume] << std::endl;
         int curVolume = totalVolume;
-        int curItem = memoN[0][curVolume];
-        while(curItem != -1)
+        for(int i = 0 ; i < itemCount; i++)
         {
-            std::cout << item[curItem].name << std::endl;
-            curVolume -= item[curItem].volume;
-            curItem = memoN[curItem][curVolume];
+            if(!memoT[i][curVolume])    continue;
+            std::cout << item[i].name << std::endl;
+            curVolume -= item[i].volume;
         }
     }
     return 0;
