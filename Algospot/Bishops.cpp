a3459356744f8fde2aa6ecafcd4d4e48aea4c069
“INJUNE <jjune1416@gmail.com>
2018-02-05 01:40:40 +0900

    pruning

--- Algospot/Bishops.cpp
+++ Algospot/Bishops.cpp
@@ -10,38 +10,56 @@
 
 const int MAX_GRID = 9;
 char grid[MAX_GRID][MAX_GRID];
-int gridSize;
 
-void SetBishop(int Y, int X, int add)
+int gridSize;
+int availableSpace;
+int maxBishop;
+
+int SetBishop(int Y, int X, int add)
 {
+    int ret = 0;
+    
     int _X = X - 1;
     int _Y = Y + 1;
     while(_X >= 0 && _Y < gridSize && grid[_Y][_X] != '*')
+    {
+        ret += !grid[_Y][_X];
         grid[_Y++][_X--] += add;
+    }
     
     _X = X + 1;
     _Y = Y + 1;
     while(_X < gridSize && _Y < gridSize && grid[_Y][_X] != '*')
+    {
+        ret += !grid[_Y][_X];
         grid[_Y++][_X++] += add;
+    }
+    return ret;
 }
 
-
-int HowManyWays(int index)
+void HowManyWays(int index, int bishopCount)
 {
-    if(index == gridSize * gridSize)    return 0;
-    int ret = HowManyWays(index + 1);
+    if(index == gridSize * gridSize)
+    {
+        maxBishop = std::max(bishopCount, maxBishop);
+        return;
+    }
+    if(bishopCount + availableSpace <= maxBishop)
+        return;
+    
+    HowManyWays(index + 1, bishopCount);
     
     int X = index % gridSize;
     int Y = index / gridSize;
     
     if(!grid[Y][X])
     {
-        SetBishop(Y, X, 1);
-        ret = std::max(ret, 1 + HowManyWays(index + 1));
+        int disabledCount = SetBishop(Y, X, 1);
+        availableSpace -= disabledCount;
+        HowManyWays(index + 1, bishopCount + 1);
         SetBishop(Y, X, -1);
+        availableSpace += disabledCount;
     }
-    
-    return ret;
 }
 
 int main()
@@ -53,10 +71,20 @@ int main()
         std::cin >> gridSize;
         for(int i = 0 ;i < gridSize ; i++)
             std::cin >> grid[i];
+        availableSpace = maxBishop = 0;
         for(int i = 0 ;i < gridSize ; i++)
+        {
             for(int j = 0 ; j< gridSize ;j++)
-                grid[i][j] = (grid[i][j] == '*' ? '*' : 0);
-        std::cout << HowManyWays(0) << std::endl;
+            {
+                if(grid[i][j] == '.')
+                {
+                    availableSpace++;
+                    grid[i][j] = 0;
+                }
+            }
+        }
+        HowManyWays(0, 0);
+        std::cout << maxBishop << std::endl;
     }
     return 0;
 }
