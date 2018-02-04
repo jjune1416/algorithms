499d7bde2143a8ced477322667e235576d1ab5f0
“INJUNE <jjune1416@gmail.com>
2018-02-05 00:37:31 +0900

    optimize

--- Algospot/KLIS.cpp
+++ Algospot/KLIS.cpp
@@ -19,22 +19,21 @@ int arr[MAX_LEN];
 int memoLIS[MAX_LEN];
 int memoCLIS[MAX_LEN];
 
-struct item_count
+struct container
 {
     int index;
     int value;
     int count;
-    item_count(int in, int va, int c):index(in),value(va),count(c){};
+    container(int in, int va, int c):index(in),value(va),count(c){};
 };
 
-bool operator<(const item_count& lhs, const item_count& rhs)
+bool operator<(const container& lhs, const container& rhs)
 {
     return lhs.value < rhs.value;
 }
 
 int LIS(int index)
 {
-    if(index < 0 || index > length)     return 0;
     int& ret = memoLIS[index];
     if(ret != -1) return ret;
     int max = 0;
@@ -46,28 +45,14 @@ int LIS(int index)
 
 int HowManyLIS(int index)
 {
-    if(index < 0 || index > length)     return 0;
     int& ret = memoCLIS[index];
     if(ret != -1)   return ret;
     if(LIS(index) == 1) return ret = 1;
+    
     ret = 0;
-    int max = 0;
     for(int i = index + 1 ;i <= length ; i++)
-    {
-        if(arr[i] > arr[index])
-        {
-            int buf = LIS(i);
-            if (buf > max)
-            {
-                max = buf;
-                ret = HowManyLIS(i);
-            }
-            else if(buf == max)
-            {
-                ret = std::min<long long>(INF, (long long)ret +  HowManyLIS(i));
-            }
-        }
-    }
+        if(arr[i] > arr[index] && LIS(index) == LIS(i) + 1)
+            ret = std::min<long long>(INF, (long long)ret +  HowManyLIS(i));
     return ret;
 }
 
@@ -75,10 +60,10 @@ std::string KLIS(int index, int lisLen, int k)
 {
     if(lisLen == 1)    return std::to_string(arr[index]);
     
-    std::vector<item_count> counts;
+    std::vector<container> counts;
     for(int i = index + 1; i <= length ; i++)
         if(arr[i] > arr[index] && LIS(i) == lisLen - 1)
-            counts.push_back(item_count(i, arr[i], HowManyLIS(i)));
+            counts.push_back(container(i, arr[i], HowManyLIS(i)));
     
     std::sort(counts.begin(), counts.end());
     int next;
