44e8d7d81ed685bf75424f4d2338ef85b7aa94ac
“INJUNE <jjune1416@gmail.com>
2018-02-11 01:56:04 +0900

    improve time complexity

--- Algospot/Restore.cpp
+++ Algospot/Restore.cpp
@@ -11,56 +11,72 @@
 #include <cstring>
 #include <string>
 
-const int MAX_WORD_COUNT = 15;
-const int MAX_WORD_LEN = 20;
+const int MAX_WORD_COUNT = 16;
 
 int wordCount;
 std::string word[MAX_WORD_COUNT];
-std::string memo[1<<15];
+int memo[1<<MAX_WORD_COUNT][MAX_WORD_COUNT];
+int dupLength[MAX_WORD_COUNT][MAX_WORD_COUNT];
 
-std::string GetShortestWord(int depth, int bitMask)
+int GetDupleicatedLength(int indexLeft, int indexRight)
 {
-    if(depth == wordCount)  return "";
+    int& ret = dupLength[indexLeft][indexRight];
+    if (ret != -1)  return ret;
     
-    std::string& ret = memo[bitMask];
-    if(ret != "")   return ret;
+    std::string left = word[indexLeft];
+    std::string right = word[indexRight];
     
-    ret += std::string(15 * 20 + 1, 'a');
-    for(int taken = 0 ; taken < wordCount ;taken++)
+    /*
+    for(int pivot = 0 ; pivot + left.length() <= right.length(); pivot++)
+        if(left == right.substr(pivot, left.length()))
+            return ret = (int)left.length();
+    
+    if(right.find(left) != -1)
+        return ret = (int)left.length();
+     */
+    
+    for(int dupLen = (int)(std::min(left.length(), right.length())); dupLen > 0 ; dupLen--)
+        if(left.substr(left.length() - dupLen) == right.substr(0, dupLen))
+            return ret = dupLen;
+    
+    return ret = 0;
+}
+
+int GetShortestLength(int before, int bitMask)
+{
+    if(bitMask == (1 << (wordCount + 1)) - 1)  return (int)word[before].length();
+    
+    int& ret = memo[bitMask][before];
+    if(ret != -1)   return ret;
+    
+    ret = 15 * 20 + 1;
+    for(int taken = 1 ; taken <= wordCount ;taken++)
+    {
+        if(bitMask & 1 << taken)    continue;
+
+        int dup = GetDupleicatedLength(before, taken);
+        int rightLength = GetShortestLength(taken, bitMask | 1 << taken);
+        ret = std::min<int>(ret, (int)word[before].length() + rightLength - dup);
+    }
+    return ret;
+}
+
+std::string GetShortestWord(int before, int bitMask)
+{
+    if(bitMask == (1 << (wordCount + 1)) - 1)  return word[before];
+    
+    int index = 0;
+    for(int taken = 1 ; taken <= wordCount ; taken++)
     {
         if(bitMask & 1 << taken)    continue;
         
-        std::string left = word[taken];
-        std::string right = GetShortestWord(depth + 1, bitMask | 1 << taken);
-
-        std::string candidate;
-        // sub string
-        for(int pivot = 0 ; pivot + left.length() <= std::min(MAX_WORD_LEN, int(right.length())); pivot++)
-        {
-            if(left == right.substr(pivot, left.length()))
-            {
-                candidate = right;
-                break;
-            }
-        }
-        if(candidate == "")
-        {
-            // 겹침
-            for(int dupLen = static_cast<int>(std::min(left.length(), right.length())); dupLen >= 0 ; dupLen--)
-            {
-                if(left.substr(left.length() - dupLen, dupLen) == right.substr(0, dupLen))
-                {
-                    candidate = left.substr(0, left.length() - dupLen) + right;
-                    break;
-                }
-            }
-        }
-        
-        if (candidate.length() < ret.length())
-            ret = candidate;
-        continue;
+        int dup = GetDupleicatedLength(before, taken);
+        int rightLength = GetShortestLength(taken, bitMask | 1 << taken);
+        if(GetShortestLength(before, bitMask) == (int)word[before].length() + rightLength - dup)
+            index = taken;
     }
-    return ret;
+    return word[before].substr(0, word[before].length() - GetDupleicatedLength(before, index))
+            + GetShortestWord(index, bitMask | 1 << index);
 }
 
 int main()
@@ -70,11 +86,18 @@ int main()
     while(TC--)
     {
         std::cin >> wordCount;
-        for(int i = 0 ;i < wordCount ; i++)
+        for(int i = 1 ;i <= wordCount ; i++)
             std::cin >> word[i];
-        for(int i = 0 ;i < 1<<15; i++)
-            memo[i].clear();
-        std::cout << GetShortestWord(0, 0) << std::endl;
+        memset(memo, -1, sizeof(memo));
+        memset(dupLength, -1, sizeof(dupLength));
+        
+        int mask = 1;
+        for(int i = 1; i <= wordCount ; i++)
+            for(int j = 1; j <= wordCount ; j++)
+                if(i != j && word[i].find(word[j]) != -1 && !(word[i] == word[j] && i > j))
+                    mask |= 1 << j;
+        
+        std::cout << GetShortestWord(0, mask) << std::endl;
     }
     return 0;
 }
