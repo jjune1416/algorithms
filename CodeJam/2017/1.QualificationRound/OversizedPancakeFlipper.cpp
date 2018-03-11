37290224181a99e419ce3dbacf79b2d3f5455a4f
“INJUNE <jjune1416@gmail.com>
2018-03-11 16:39:38 +0900

    Oversize pancake flipper initial commit

---
+++ CodeJam/2017/1.QualificationRound/OversizedPancakeFlipper.cpp
@@ -0,0 +1,56 @@
+//
+//  Qualification Round 2017 Probalem A
+//  https://code.google.com/codejam/contest/3264486/dashboard
+//
+//  Created by Ghost on 2018. 3. 11..
+//  Copyright 2018 Ghost. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+
+inline void flip(std::string& pancakes, int index, int S)
+{
+    while(S--)
+    {
+        pancakes[index] = (pancakes[index] == '-' ? '+' : '-');
+        index++;
+    }
+}
+
+int FlipTimes(std::string pancakes, int S)
+{
+    int times = 0;
+    for(int index = 0 ; index + S <= pancakes.length() ; index++)
+    {
+        if(pancakes[index] == '-')
+        {
+            times++;
+            flip(pancakes, index, S);
+        }
+    }
+    
+    for(int index = 0 ; index < pancakes.length() ; index++)
+        if(pancakes[index] == '-')
+            return -1;
+    return times;
+}
+
+int main()
+{
+    int TC;
+    std::cin >> TC;
+    for(int i = 0 ;i < TC ; i++)
+    {
+        int S;
+        std::string pancakes;
+        std::cin >> pancakes >> S;
+        int ans = FlipTimes(pancakes, S);
+        std::cout << "Case #" << i + 1 << ": ";
+        if(ans == -1)
+            std::cout << "IMPOSSIBLE";
+        else
+            std::cout << ans;
+        std::cout << std::endl;
+    }
+}
